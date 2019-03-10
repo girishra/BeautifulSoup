fdc8bc32090cb8fabeade426d02978ee91d3e7db
girishra <34037021+girishra@users.noreply.github.com>
2019-03-11 00:17:17 +0530

    Add files via upload

---
+++ vincian.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+result=requests.get("https://vin01.tk/")
+print(result)
+src=result.content
+soup=BeautifulSoup(src,'lxml')
+company=[]
+selects = soup.findAll('font')
+for match in selects:
+        match.decompose()
+for categories in soup.div.center.find_all('a'):
+        categories=categories.get_text().strip()
+        company.append(categories)
+print(company)
