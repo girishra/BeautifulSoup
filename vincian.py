aaff00282e8932c131db40ac8b083737289bf087
girishra <34037021+girishra@users.noreply.github.com>
2019-03-10 23:56:14 +0530

    Update vincian.py

--- vincian.py
+++ vincian.py
@@ -1,14 +1,14 @@
-1.from bs4 import BeautifulSoup
-2.import requests
-3.result=requests.get("https://vin01.tk/")
-4.result
-5.src=result.content
-6.soup=BeautifulSoup(src,'lxml')
-7.company=[]
-8.selects = soup.findAll('select')
-9.for match in selects:
+from bs4 import BeautifulSoup
+import requests
+result=requests.get("https://vin01.tk/")
+result
+src=result.content
+soup=BeautifulSoup(src,'lxml')
+company=[]
+selects = soup.findAll('select')
+for match in selects:
         match.decompose()
-10.for categories in soup.div.center.find_all('a'):
+for categories in soup.div.center.find_all('a'):
         categories=categories.get_text()
         company.append(categories)
-11.company
+company
